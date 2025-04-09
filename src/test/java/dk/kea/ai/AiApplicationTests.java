59ec9c4d1f6a5e9642b35c35e6fb1f2b3bd9e07d
sebwelsch <sebjak1@hotmail.com>
2025-04-09 10:18:13 +0200

    inital spring boot

---
+++ src/test/java/dk/kea/ai/AiApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
